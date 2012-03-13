02fa448745e368e41aa238536dd0d9c8b5c95014
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-03-13 17:28:53 +0800

    Iniialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCapLinodeApp::Application.config.secret_token = 'c6bec859306396ee72abeb758afc4e627aa7017930a5fffffa478bd9debd79812914d94ac2432b5937aae582e202cac52e066bf06a9bb5b702ee03e6cef71f92'
